30cde63055b535d65c7c5225480a4bbf978315c2
 User <user@MacBook-Pro.local>
2024-04-20 07:05:45 +0300

    add demo weather app

---
+++ DemoWeatherApp/DemoWeatherApp/WeatherController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoWeatherApp
+//
+//  Created by  User on 20.04.2024.
+//
+
+import UIKit
+
+class WeatherController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
